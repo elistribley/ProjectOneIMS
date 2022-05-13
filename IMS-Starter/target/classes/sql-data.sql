bd7e1de7c6ce8dcddb02e00de76a8850b5de460a
elistribley <eli_stribley@hotmail.com>
2022-05-13 13:10:28 +0100

    got order read working and showing item price now! final upload ES

---
+++ IMS-Starter/target/classes/sql-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `Customers` (`first_name`, `surname`, `address`) VALUES ('jordan', 'harrison', '64 Zoo Lane');
+INSERT INTO `Orders` (`id_customer`) VALUES (1);
+INSERT INTO `Orders` (`id_customer`) VALUES (2);
+INSERT INTO `Items` (`item_name`, `item_price`) VALUES ('Book', 8.99);
+INSERT INTO `OrderItems` (`id_order`, `item_id`) VALUES (1, 1);
